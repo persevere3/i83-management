61f86ee5b15a85e042ed69dc69da51723b20569d
Jacob0512 <psves0512@gmail.com>
2024-03-25 09:29:11 +0800

    router

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,6 +9,7 @@ const Layouts = () => import("@/layouts/index.vue")
  * 除了 redirect/403/404/login 等隐藏页面，其他页面建议设置 Name 属性
  */
 export const constantRoutes: RouteRecordRaw[] = [
+  // ?
   {
     path: "/redirect",
     component: Layouts,
@@ -22,6 +23,7 @@ export const constantRoutes: RouteRecordRaw[] = [
       }
     ]
   },
+  // 403
   {
     path: "/403",
     component: () => import("@/views/error-page/403.vue"),
@@ -29,6 +31,7 @@ export const constantRoutes: RouteRecordRaw[] = [
       hidden: true
     }
   },
+  // 404
   {
     path: "/404",
     component: () => import("@/views/error-page/404.vue"),
@@ -37,140 +40,14 @@ export const constantRoutes: RouteRecordRaw[] = [
     },
     alias: "/:pathMatch(.*)*"
   },
+  // login
   {
     path: "/login",
     component: () => import("@/views/login/index.vue"),
     meta: {
       hidden: true
     }
-  },
-  {
-    path: "/",
-    component: Layouts,
-    redirect: "/dashboard",
-    children: [
-      {
-        path: "dashboard",
-        component: () => import("@/views/dashboard/index.vue"),
-        name: "Dashboard",
-        meta: {
-          title: "首頁",
-          svgIcon: "dashboard",
-          keepAlive: true
-        }
-      }
-    ]
-  },
-  {
-    path: "/management",
-    component: Layouts,
-    redirect: "/management/category-list",
-    name: "Management",
-    meta: {
-      title: "管理",
-      elIcon: "Grid"
-    },
-    children: [
-      {
-        path: "category-list",
-        component: () => import("@/views/management/category-list/index.vue"),
-        name: "CategoryList",
-        meta: {
-          title: "分類管理",
-          keepAlive: true
-        }
-      },
-      {
-        path: "select-list",
-        component: () => import("@/views/management/select-list/index.vue"),
-        name: "SelectList",
-        meta: {
-          title: "選擇管理",
-          keepAlive: true
-        }
-      },
-      {
-        path: "meal-list",
-        component: () => import("@/views/management/meal-list/index.vue"),
-        name: "MealList",
-        meta: {
-          title: "餐點管理",
-          keepAlive: true
-        }
-      },
-      {
-        path: "table-list",
-        component: () => import("@/views/management/table-list/index.vue"),
-        name: "TabletList",
-        meta: {
-          title: "桌號管理",
-          keepAlive: true
-        }
-      },
-      {
-        path: "order-list",
-        component: () => import("@/views/management/order-list/index.vue"),
-        name: "OrdertList",
-        meta: {
-          title: "訂單管理",
-          keepAlive: true
-        }
-      }
-    ]
-  },
-  {
-    path: "/customer",
-    component: Layouts,
-    redirect: "/management/table-list",
-    meta: {
-      hidden: true,
-      title: "QR code"
-    },
-    children: [
-      {
-        path: ":id",
-        component: () => import("@/views/customer/index.vue"),
-        name: "customer"
-      }
-    ]
   }
-  // {
-  //   path: "/hook-demo",
-  //   component: Layouts,
-  //   redirect: "/hook-demo/use-fetch-select",
-  //   name: "HookDemo",
-  //   meta: {
-  //     title: "Hook 示例",
-  //     elIcon: "Menu",
-  //     alwaysShow: true
-  //   },
-  //   children: [
-  //     {
-  //       path: "use-fetch-select",
-  //       component: () => import("@/views/hook-demo/use-fetch-select.vue"),
-  //       name: "UseFetchSelect",
-  //       meta: {
-  //         title: "useFetchSelect"
-  //       }
-  //     },
-  //     {
-  //       path: "use-fullscreen-loading",
-  //       component: () => import("@/views/hook-demo/use-fullscreen-loading.vue"),
-  //       name: "UseFullscreenLoading",
-  //       meta: {
-  //         title: "useFullscreenLoading"
-  //       }
-  //     },
-  //     {
-  //       path: "use-watermark",
-  //       component: () => import("@/views/hook-demo/use-watermark.vue"),
-  //       name: "UseWatermark",
-  //       meta: {
-  //         title: "useWatermark"
-  //       }
-  //     }
-  //   ]
-  // }
 ]
 
 /**
@@ -179,37 +56,205 @@ export const constantRoutes: RouteRecordRaw[] = [
  * 必须带有 Name 属性
  */
 export const asyncRoutes: RouteRecordRaw[] = [
-  // {
-  //   path: "/permission",
-  //   component: Layouts,
-  //   redirect: "/permission/page",
-  //   name: "Permission",
-  //   meta: {
-  //     title: "權限管理",
-  //     svgIcon: "lock",
-  //     roles: ["admin", "editor"], // 可以在根路由中设置角色
-  //     alwaysShow: true // 将始终显示根菜单
-  //   },
-  //   children: [
-  //     {
-  //       path: "page",
-  //       component: () => import("@/views/permission/page.vue"),
-  //       name: "PagePermission",
-  //       meta: {
-  //         title: "頁面權限",
-  //         roles: ["admin"] // 或者在子导航中设置角色
-  //       }
-  //     },
-  //     {
-  //       path: "directive",
-  //       component: () => import("@/views/permission/directive.vue"),
-  //       name: "DirectivePermission",
-  //       meta: {
-  //         title: "指令權限" // 如果未设置角色，则表示：该页面不需要权限，但会继承根路由的角色
-  //       }
-  //     }
-  //   ]
-  // },
+  // /
+  {
+    path: "/",
+    component: Layouts,
+    redirect: "/dashboard",
+    meta: {
+      roles: ["super-admin", "branch-admin"]
+    },
+    children: [
+      {
+        path: "dashboard",
+        component: () => import("@/views/dashboard/index.vue"),
+        name: "Dashboard",
+        meta: {
+          title: "銷售報表",
+          svgIcon: "dashboard"
+        }
+      }
+    ]
+  },
+  // account
+  {
+    path: "/",
+    component: Layouts,
+    redirect: "/account-list",
+    meta: {
+      roles: ["super-admin"]
+    },
+    children: [
+      {
+        path: "account-list",
+        component: () => import("@/views/management/account-list/index.vue"),
+        name: "AccountList",
+        meta: {
+          title: "帳戶管理",
+          svgIcon: "user"
+        }
+      }
+    ]
+  },
+  // store
+  {
+    path: "/",
+    component: Layouts,
+    redirect: "/store-list",
+    meta: {
+      roles: ["super-admin"]
+    },
+    children: [
+      {
+        path: "store-list",
+        component: () => import("@/views/management/store-list/index.vue"),
+        name: "StoreList",
+        meta: {
+          title: "分店管理",
+          svgIcon: "store"
+        }
+      }
+    ]
+  },
+  // pay method
+  {
+    path: "/",
+    component: Layouts,
+    redirect: "/payMethod",
+    meta: {
+      roles: ["super-admin"]
+    },
+    children: [
+      {
+        path: "payMethod",
+        component: () => import("@/views/management/payMethod/index.vue"),
+        name: "PayMethod",
+        meta: {
+          title: "支付管理",
+          svgIcon: "cash"
+        }
+      }
+    ]
+  },
+  // category
+  {
+    path: "/",
+    component: Layouts,
+    redirect: "/category-list",
+    meta: {
+      roles: ["super-admin"]
+    },
+    children: [
+      {
+        path: "category-list",
+        component: () => import("@/views/management/category-list/index.vue"),
+        name: "CategoryList",
+        meta: {
+          title: "分類管理",
+          svgIcon: "category",
+          keepAlive: true
+        }
+      }
+    ]
+  },
+  // select
+  {
+    path: "/",
+    component: Layouts,
+    redirect: "/select-list",
+    meta: {
+      roles: ["super-admin"]
+    },
+    children: [
+      {
+        path: "select-list",
+        component: () => import("@/views/management/select-list/index.vue"),
+        name: "SelectList",
+        meta: {
+          title: "選擇管理",
+          svgIcon: "select",
+          keepAlive: true
+        }
+      }
+    ]
+  },
+  // meal
+  {
+    path: "/",
+    component: Layouts,
+    redirect: "/meal-list",
+    meta: {
+      roles: ["super-admin", "branch-admin"]
+    },
+    children: [
+      {
+        path: "meal-list",
+        component: () => import("@/views/management/meal-list/index.vue"),
+        name: "MealList",
+        meta: {
+          title: "餐點管理",
+          svgIcon: "meal",
+          keepAlive: true
+        }
+      }
+    ]
+  },
+  // table
+  {
+    path: "/",
+    component: Layouts,
+    redirect: "/table-list",
+    meta: {
+      roles: ["super-admin", "branch-admin"]
+    },
+    children: [
+      {
+        path: "table-list",
+        component: () => import("@/views/management/table-list/index.vue"),
+        name: "TabletList",
+        meta: {
+          title: "桌號管理",
+          svgIcon: "table",
+          keepAlive: true
+        }
+      }
+    ]
+  },
+  // order
+  {
+    path: "/",
+    component: Layouts,
+    redirect: "/order-list",
+    children: [
+      {
+        path: "order-list",
+        component: () => import("@/views/management/order-list/index.vue"),
+        name: "OrdertList",
+        meta: {
+          title: "訂單管理",
+          svgIcon: "order"
+        }
+      }
+    ]
+  },
+  // customer
+  {
+    path: "/",
+    component: Layouts,
+    redirect: "/customer/:id",
+    meta: {
+      hidden: true
+    },
+    children: [
+      {
+        path: "customer/:id",
+        component: () => import("@/views/customer/index.vue"),
+        name: "Customer"
+      }
+    ]
+  },
+
+  // ?
   {
     path: "/:pathMatch(.*)*", // Must put the 'ErrorPage' route at the end, 必须将 'ErrorPage' 路由放在最后
     redirect: "/404",
